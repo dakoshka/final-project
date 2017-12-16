d7aff3ae1364bd581782daa7749145514cedd4c2
DJ Save <dako.sidamonidze@gmail.com>
2017-12-16 11:42:14 +0400

    initial commit

---
+++ js/js-custom.js
@@ -0,0 +1,47 @@
+var i = 0;
+var txt = 'Every New Year gives you the perfect chance to start something new and fresh. So do your bit this year and make the world a better place for yourself and others. Happy New Year!';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
+var countDownDate = new Date("jan 1, 2018 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("clock").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("clock").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+
+function submitClicked(){
+  alert('Message Sent!');
+  return false;
+}
+
